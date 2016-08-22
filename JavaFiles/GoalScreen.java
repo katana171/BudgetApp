7b99e3ec8d08437a3f98a3b816eca14b25f9a0cb
Tanaka Ruzvidzo <sheriuz@hotmail.com>
2016-08-22 12:35:03 -0400

    Add files via upload

---
+++ JavaFiles/GoalScreen.java
@@ -0,0 +1,14 @@
+package com.lightningstudio.budgetapp;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class GoalScreen extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goal_screen);
+    }
+
+}
